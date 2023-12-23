5b50bf5ecc310774e6401a28585c2bd8a0628c8c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 17:56:37 +0530

    add: db config env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+import path from "path";
 
-export const { PORT, NODE_ENV } = process.env;
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+});
+
+export const {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_NAME,
+} = process.env;
+
 export { default as logger } from "./logger";
+export { default as AppDataSource } from "./data-source";
