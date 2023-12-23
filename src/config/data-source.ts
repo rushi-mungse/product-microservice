cfe417b05257dd54a4b46144b2a10e9db2c306df
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-23 17:56:19 +0530

    add: data-source for db

---
+++ src/config/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Product } from "../entity";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./";
+
+export default new DataSource({
+    type: "postgres",
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    synchronize: false,
+    logging: false,
+    entities: [Product],
+    migrations: [],
+    subscribers: [],
+});
