d0a451c3aa52d0e912cdd1f291ea2ab990f2a496
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-04 09:16:28 +0530

    add: axios api

---
+++ src/apis/index.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+export default api;
