a0cbc33f32bb1881774ed55b72178ab6a17babe8
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-04 09:15:21 +0530

    add: cookie parser adn product router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import "reflect-metadata";
 import express from "express";
 import { errorHandlerMiddleware } from "./middlewares";
+import { productRouter } from "./routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
+
+app.use("/api/product", productRouter);
 
 app.use(errorHandlerMiddleware);
 
