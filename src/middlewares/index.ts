312caca75091b958782a9123ff26055c2f7745f0
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-04 15:53:27 +0530

    add: multer image processing middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as errorHandlerMiddleware } from "./error.middleware";
 export { default as checkAccessToken } from "./access.middleware";
 export { default as hasPermission } from "./permission.middleware";
+export { default as upload } from "./multer.middleware";
