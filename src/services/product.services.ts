41aba362328efa9640162a2c1d44edde535e01bd
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-04 17:46:42 +0530

    add: upload file method

--- src/services/product.services.ts
+++ src/services/product.services.ts
@@ -1,12 +1,20 @@
 import { Repository } from "typeorm";
 import { Product } from "../entity";
 import { IProduct } from "../types";
+import { UploadApiResponse } from "cloudinary";
 
 class ProductService {
-    constructor(private productRepository: Repository<Product>) {}
+    constructor(
+        private productRepository: Repository<Product>,
+        private uploadeOnCloudinary: (T: string) => Promise<UploadApiResponse>,
+    ) {}
 
     async create(product: IProduct) {
         return await this.productRepository.save(product);
+    }
+
+    async uploadFile(localFilePath: string) {
+        return await this.uploadeOnCloudinary(localFilePath);
     }
 }
 
