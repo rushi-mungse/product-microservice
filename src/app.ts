679c1fe2839df918d842b7e1dcaa8e75e91df359
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-04 15:49:31 +0530

    add: express middleware

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/product", productRouter);
 app.use("/api/category", categoryRouter);
